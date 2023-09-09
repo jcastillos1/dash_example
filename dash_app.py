eccf9c4484083cdd87f1f82b31f5e4bb28289ced
Juan Castillo <64432778+jcastillos1@users.noreply.github.com>
2023-09-08 19:04:24 -0500

    Update dash_app.py

--- dash_app.py
+++ dash_app.py
@@ -2,40 +2,48 @@ from api import data_api
 from transforming import transforming
 from dash.dependencies import Input, Output, State
 from dash import dcc, html
+from io import BytesIO
+import requests, base64
 import dash, time
 
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
+tokens = {'test@test.com': 2}
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
+url = "https://media.licdn.com/dms/image/C560BAQEkLah7iU0OXg/company-logo_200_200/0/1622576378267?e=1701907200&v=beta&t=jHOKiWYubvJbIHNss3cvWPgbf5Rv_427P9tq7jcd5pY"
+response = requests.get(url)
+image_bytes = BytesIO(response.content)
+encoded_image = base64.b64encode(image_bytes.read()).decode("utf-8")
 app.layout = html.Div([
-    html.Img(src='https://media.licdn.com/dms/image/C560BAQEkLah7iU0OXg/company-logo_200_200/0/1622576378267?e=1701907200&v=beta&t=jHOKiWYubvJbIHNss3cvWPgbf5Rv_427P9tq7jcd5pY', style={'display': 'block', 'margin-left': 'auto', 'margin-right': 'auto'}),
-    html.H1('Generador automático de informes', style={'width': '100%', 'color': '#878786', 'margin-top': '-10px', 'textAlign': 'center', 'font-family': 'Arial, sans-serif'}),
-    dcc.Input(id='email', type='email', placeholder='Ingrese su correo', style={'width': '100%', 'height': '50px', 'margin-top': '10px'}),
-    dcc.Input(id='password', type='password', placeholder='Ingrese su contraseña', style={'width': '100%', 'height': '50px', 'margin-top': '10px'}),
-    dcc.Input(id='client', type='text', placeholder='Ingrese el cliente', style={'width': '100%', 'height': '50px', 'margin-top': '10px'}),
-    html.Button('Run Dash', id='login-button', n_clicks=0, style={'width': '101.5%', 'height': '50px', 'margin-top': '10px'}),
-    html.Div(id='output-container-button', style={'margin-top': '20px'})
-], style={'width': '30%', 'margin-left': 'auto', 'margin-right': 'auto'})
+    html.Img(src='data:image/png;base64,{}'.format(encoded_image), style={'width':'12.5%', 'float':'left', 'margin-left':'150px', 'margin-top':'-35px'}),
+    dcc.Input(id='email', type='email', placeholder='Ingrese su correo'),
+    dcc.Input(id='client', type='text', placeholder='Ingrese el cliente'),
+    html.Button('Generar informe', id='generate_button', n_clicks=0, style={'width':'12.5%'}),
+    html.Div(id='text_info'),
+    html.Div(id='title_output'),
+    html.Div(id='dash_output'),
+], style={'width':'100%', 'text-align':'center'})
 @app.callback(
-    Output('output-container-button', 'children'),
-    [Input('login-button', 'n_clicks')],
-    [State('email', 'value'),
-     State('password', 'value'),
-     State('client', 'value')]
+    Output('text_info', 'children'),
+    Output('title_output', 'children'),
+    Output('dash_output', 'children'),
+    Input('generate_button', 'n_clicks'),
+    State('email', 'value'),
+    State('client', 'value')
 )
-def update_output(n_clicks, email, password, client):
-    if n_clicks != 0:
+def update_output(n_clicks, email, client):
+    if n_clicks  != 0:
         start_time = time.time()
-        if email == 'test@test.com' and password == 'password':
+        if email in tokens.keys() and tokens[email] >= 1:
             data = data_api(client)
-            layout_dashboard = transforming(client, data)
-            app.layout = layout_dashboard
+            title_output, dash_output = transforming(client, data)
             execution_time = round(time.time() - start_time, 2)
-            return dcc.Link('Go to Dashboard', href=f'/Dashboard_{client}'), html.P(f'Execution time: {execution_time} seconds')
+            tokens[email] -= 1
+            return f'Tiempo de ejecución: {execution_time}s',title_output, dash_output
         else:
-            return 'Access denied. Please check your credentials.'
+            return 'Acceso denegado. Verifica tus credenciales o revisa tus tokens.', None, None
     else:
-        return html.P('Tiempo estimado para generar el Dash: 20s')
-    
+        return 'Tiempo estimado en generar Dash: 20s', None, None
 if __name__ == '__main__':
     app.run_server(debug=True)
