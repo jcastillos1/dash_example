82976b3e045c06f6139d14e33a1cedc731dea15a
Juan Castillo <64432778+jcastillos1@users.noreply.github.com>
2023-09-20 15:49:21 -0500

    Update dash_app.py

--- dash_app.py
+++ dash_app.py
@@ -1,49 +1,51 @@
-from api import data_api
-from transforming import transforming
-from dash.dependencies import Input, Output, State
-from dash import dcc, html
-from io import BytesIO
-import requests, base64
-import dash, time
+import grpc
+import partner_api2_pb2_grpc as api
+from partner_api2_pb2 import *
+import pandas as pd
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+import numpy as np
 
+def data_api(cliente):
+    partnerApiEndpoint = 'partner.emporiaenergy.com:50052'
+    creds = grpc.ssl_channel_credentials()
+    channel = grpc.secure_channel(partnerApiEndpoint, creds)
+    stub = api.PartnerApiStub(channel)
+    request = AuthenticationRequest()
+    request.partner_email = 'hsoto@cigepty.com'
+    request.password = 'hsm280466'
+    auth_response = stub.Authenticate(request=request)
+    auth_token = auth_response.auth_token
 
-tokens = {'test@test.com': 10}
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-url = "https://media.licdn.com/dms/image/C560BAQEkLah7iU0OXg/company-logo_200_200/0/1622576378267?e=1701907200&v=beta&t=jHOKiWYubvJbIHNss3cvWPgbf5Rv_427P9tq7jcd5pY"
-response = requests.get(url)
-image_bytes = BytesIO(response.content)
-encoded_image = base64.b64encode(image_bytes.read()).decode("utf-8")
-app.layout = html.Div([
-    html.Img(src='data:image/png;base64,{}'.format(encoded_image), style={'width':'12.5%', 'float':'left', 'margin-left':'150px', 'margin-top':'-35px'}),
-    dcc.Input(id='email', type='email', placeholder='Ingrese su correo'),
-    dcc.Input(id='client', type='text', placeholder='Ingrese el cliente'),
-    html.Button('Generar informe', id='generate_button', n_clicks=0, style={'width':'12.5%'}),
-    html.Div(id='text_info'),
-    html.Div(id='title_output'),
-    html.Div(id='dash_output'),
-], style={'width':'100%', 'text-align':'center'})
-@app.callback(
-    Output('text_info', 'children'),
-    Output('title_output', 'children'),
-    Output('dash_output', 'children'),
-    Input('generate_button', 'n_clicks'),
-    State('email', 'value'),
-    State('client', 'value')
-)
-def update_output(n_clicks, email, client):
-    if n_clicks  != 0:
-        start_time = time.time()
-        if email in tokens.keys() and tokens[email] >= 1:
-            data = data_api(client)
-            title_output, dash_output = transforming(client, data)
-            execution_time = round(time.time() - start_time, 2)
-            tokens[email] -= 1
-            return f'Tiempo de ejecución: {execution_time}s',title_output, dash_output
-        else:
-            return 'Acceso denegado. Verifica tus credenciales o revisa tus tokens.', None, None
-    else:
-        return 'Tiempo estimado en generar Dash: 20s', None, None
-if __name__ == '__main__':
-    app.run_server(debug=True)
+    inventoryRequest = DeviceInventoryRequest()
+    inventoryRequest.auth_token = auth_token
+    inventoryResponse = stub.GetDevices(inventoryRequest)
+
+    deviceUsageRequest = DeviceUsageRequest()
+    deviceUsageRequest.auth_token = auth_token
+    date_range = pd.date_range(start=datetime.now()-relativedelta(days=90), end=datetime.now()-relativedelta(days=1))
+    now = int((datetime.now().replace(microsecond=0) + timedelta(seconds=1)).timestamp())
+    deviceUsageRequest.start_epoch_seconds = now - len(date_range) * 24 * 60 * 60 # one hour of seconds
+    deviceUsageRequest.end_epoch_seconds = now
+    deviceUsageRequest.scale = DataResolution.Days
+    deviceUsageRequest.channels = DeviceUsageRequest.UsageChannel.ALL
+    usageResponse = stub.GetUsageData(deviceUsageRequest)
+
+    names = {}; data = pd.DataFrame()
+    for device in inventoryResponse.devices: 
+        if device.device_name == cliente:
+            device_id = device.manufacturer_device_id
+            for circuit in device.circuit_infos:
+                if circuit.name != '': 
+                    names[circuit.channel_number] = circuit.sub_type+'-'+circuit.name
+    device_found =  [i for i in usageResponse.device_usages if i.manufacturer_device_id==device_id][0]
+    days = len(device_found.channel_usages[0].usages)
+    data['Time Bucket'] = pd.date_range(start=datetime.now()-relativedelta(days=days),end=datetime.now()-relativedelta(days=1)).date
+    for circuit in device_found.channel_usages:
+        if circuit.channel in names.keys():
+            data[f'{circuit.channel}-{names[circuit.channel]}'] = np.array(circuit.usages)/3600
+        if circuit.channel == 1: data[f'{circuit.channel}-Mains_A'] = np.array(circuit.usages)/3600
+        if circuit.channel == 2: data[f'{circuit.channel}-Mains_B'] = np.array(circuit.usages)/3600
+        if circuit.channel == 3: data[f'{circuit.channel}-Mains_C'] = np.array(circuit.usages)/3600
+
+    return data
