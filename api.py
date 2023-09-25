7f2a69dc5044afca9d5e3306c360658d746f5e3f
Juan Castillo <64432778+jcastillos1@users.noreply.github.com>
2023-09-25 11:37:03 -0500

    Update api.py

--- api.py
+++ api.py
@@ -16,11 +16,9 @@ def data_api(cliente):
     request.password = 'hsm280466'
     auth_response = stub.Authenticate(request=request)
     auth_token = auth_response.auth_token
-
     inventoryRequest = DeviceInventoryRequest()
     inventoryRequest.auth_token = auth_token
     inventoryResponse = stub.GetDevices(inventoryRequest)
-
     deviceUsageRequest = DeviceUsageRequest()
     deviceUsageRequest.auth_token = auth_token
     date_range = pd.date_range(start=datetime.now()-relativedelta(days=90), end=datetime.now()-relativedelta(days=1))
@@ -33,19 +31,18 @@ def data_api(cliente):
 
     names = {}; data = pd.DataFrame()
     for device in inventoryResponse.devices: 
-        if device.device_name == cliente:
+        if cliente.lower() in device.device_name.lower():
             device_id = device.manufacturer_device_id
             for circuit in device.circuit_infos:
                 if circuit.name != '': 
                     names[circuit.channel_number] = circuit.sub_type+'-'+circuit.name
     device_found =  [i for i in usageResponse.device_usages if i.manufacturer_device_id==device_id][0]
     days = len(device_found.channel_usages[0].usages)
-    data['Time Bucket'] = pd.date_range(start=datetime.now()-relativedelta(days=days),end=datetime.now()-relativedelta(days=1)).date
+    data['Time Bucket'] = pd.date_range(start=datetime.now()-relativedelta(days=days)+relativedelta(days=1),end=datetime.now()).date
     for circuit in device_found.channel_usages:
         if circuit.channel in names.keys():
-            data[f'{circuit.channel}-{names[circuit.channel]}'] = np.array(circuit.usages)/3600
-        if circuit.channel == 1: data[f'{circuit.channel}-Mains_A'] = np.array(circuit.usages)/3600
-        if circuit.channel == 2: data[f'{circuit.channel}-Mains_B'] = np.array(circuit.usages)/3600
-        if circuit.channel == 3: data[f'{circuit.channel}-Mains_C'] = np.array(circuit.usages)/3600
-
+            data[f'{circuit.channel}-{names[circuit.channel]}'] = np.array(circuit.usages)/100
+        if circuit.channel == 1: data[f'{circuit.channel}-Mains_A'] = np.array(circuit.usages)/100
+        if circuit.channel == 2: data[f'{circuit.channel}-Mains_B'] = np.array(circuit.usages)/100
+        if circuit.channel == 3: data[f'{circuit.channel}-Mains_C'] = np.array(circuit.usages)/100
     return data
