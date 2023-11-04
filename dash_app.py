1404566d868c226fba318151add7560a365d9f9e
Juan Castillo <64432778+jcastillos1@users.noreply.github.com>
2023-11-04 00:05:03 -0500

    Update dash_app.py
    v1.1

--- dash_app.py
+++ dash_app.py
@@ -17,7 +17,7 @@ url = "https://media.licdn.com/dms/image/C560BAQEkLah7iU0OXg/company-logo_200_20
 response = requests.get(url)
 image_bytes = BytesIO(response.content)
 encoded_image = base64.b64encode(image_bytes.read()).decode("utf-8")
-date_range = pd.date_range(start=datetime.now()-relativedelta(days=90), end=datetime.now()-relativedelta(days=1))
+date_range = pd.date_range(start=datetime.now()-relativedelta(days=60),end=datetime.now()-relativedelta(days=1))
 app.layout = html.Div([
     html.Img(src='data:image/png;base64,{}'.format(encoded_image), style={'width':'12.5%', 'float':'left', 'margin-left':'150px', 'margin-top':'-35px'}),
     dcc.Input(id='email', type='email', placeholder='Ingrese su correo'),
@@ -27,8 +27,8 @@ app.layout = html.Div([
             min_date_allowed=date_range.min(),
             max_date_allowed=date_range.max(),
             initial_visible_month=(datetime.now()-relativedelta(months=1)).date(),
-            start_date=(datetime.now()-relativedelta(months=1)).date(),
-            end_date=date_range.max()),
+            start_date = (datetime.now()-relativedelta(months=1)-relativedelta(days=1)).date(),
+            end_date = date_range.max()),
     html.Div(id='text_info'),
     html.Div(id='title_output'),
     html.Div(id='dash_output'),
@@ -43,18 +43,18 @@ app.layout = html.Div([
     Input('my-date-picker-range', 'end_date')
 )
 def update_output(email, client, start_date, end_date):
-    if email and client:
+    if email in ['hsoto@cigepty.com', 'info@cigepty.com'] and client:
         start_time = time.time()
         if email in ['info@cigepty.com', 'hsoto@cigepty.com']:
-            device_name, data = data_api(client)
+            device_name, data, data_hist = data_api(client)
             start_date = pd.Timestamp(start_date).to_pydatetime().date()
             end_date = pd.Timestamp(end_date).to_pydatetime().date()
-            title_output, dash_output = transforming(device_name, data, start_date, end_date)
+            title_output, dash_output = transforming(device_name, data, data_hist, start_date, end_date)
             execution_time = round(time.time() - start_time, 2)
             return f'Tiempo de ejecución: {execution_time}s',title_output, dash_output
         else:
-            return 'Acceso denegado. Verifica tus credenciales o revisa tus tokens.', None, None
+            return 'Acceso denegado, verifica tus credenciales.', None, None
     else:
-        return 'Tiempo estimado en generar Dash: 30s', None, None
+        return 'Tiempo estimado en generar informe por primera vez: 1.5min', None, None
 if __name__ == '__main__':
     app.run_server(debug=True)
