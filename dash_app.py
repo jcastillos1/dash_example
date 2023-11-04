10e5cb5c63686904e9d6b8b0401f8176d232d0aa
Juan Castillo <64432778+jcastillos1@users.noreply.github.com>
2023-11-04 15:32:22 -0500

    Update dash_app.py
    v1.1

--- dash_app.py
+++ dash_app.py
@@ -43,16 +43,16 @@ app.layout = html.Div([
     Input('my-date-picker-range', 'end_date')
 )
 def update_output(email, client, start_date, end_date): 
-    if email in ['info@cigepty.com', 'hsoto@cigepty.com'] and client!=None:
-        start_time = time.time()
-        device_name, data, data_hist = data_api(client)
-        start_date = pd.Timestamp(start_date).to_pydatetime().date()
-        end_date = pd.Timestamp(end_date).to_pydatetime().date()
-        title_output, dash_output = transforming(device_name, data, data_hist, start_date, end_date)
-        execution_time = round(time.time() - start_time, 2)
-        return f'Tiempo de ejecución: {execution_time}s', title_output, dash_output
-    if not email in ['info@cigepty.com', 'hsoto@cigepty.com']:
-        return 'Acceso denegado, verifica tus credenciales.', None, None
-
+    if email and client:
+        if email in ['info@cigepty.com', 'hsoto@cigepty.com']:
+            start_time = time.time()
+            device_name, data, data_hist = data_api(client)
+            start_date = pd.Timestamp(start_date).to_pydatetime().date()
+            end_date = pd.Timestamp(end_date).to_pydatetime().date()
+            title_output, dash_output = transforming(device_name, data, data_hist, start_date, end_date)
+            execution_time = round(time.time() - start_time, 2)
+            return f'Tiempo de ejecución: {execution_time}s', title_output, dash_output
+        else:
+            return 'Acceso denegado, verifica que sea un correo válido.', None, None
 if __name__ == '__main__':
     app.run_server(debug=True)
